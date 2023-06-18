53d58d1d07e751b3f6857d5af881407065461433
tuvieja <105611148+n1na666@users.noreply.github.com>
2023-06-17 23:29:40 -0500

    hasta el update

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
 
-app.use(bodyParser.urlencoded({ extended: true }))
 
 
 MongoClient.connect("mongodb+srv://diego:123@cluster0.ugvbjo7.mongodb.net/star-wars?retryWrites=true&w=majority", { useUnifiedTopology: true })
@@ -14,15 +13,34 @@ MongoClient.connect("mongodb+srv://diego:123@cluster0.ugvbjo7.mongodb.net/star-w
     const db = client.db('star-wars-quotes')
     const quotesCollection = db.collection('quotes')
 
+    app.set('view engine', 'ejs')
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(express.static('public'))
+    app.use(bodyParser.json())
+    
     app.listen(3000, function () {
         console.log('listening on 3000')
       })
+
+
+
+    
+
     app.get('/', (req, res) => {
       //  res.send('Hello World')
-      res.sendFile(__dirname + '/index.html')
+      //  res.sendFile(__dirname + '/index.html')
       // Note: __dirname is the current directory you're in. Try logging it and see what you get!
       // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+      db.collection('quotes')
+        .find()
+        .toArray()
+        .then(results => {
+            res.render('index.ejs', { quotes: results })
+        })
+        .catch(error => console.error(error))
+        
     })
+
     app.post('/quotes', (req, res) => {
         quotesCollection
             .insertOne(req.body)
@@ -31,7 +49,32 @@ MongoClient.connect("mongodb+srv://diego:123@cluster0.ugvbjo7.mongodb.net/star-w
             })
             .catch(error => console.error(error))
             })
+     
+            
+            
+    app.put('/quotes', (req, res) => {
+      quotesCollection
+        .findOneAndUpdate(
+          { name: 'Diego' },
+          {
+            $set: {
+              name: req.body.name,
+              quote: req.body.quote,
+            },
+          },
+          {
+            upsert: true,
+          }
+        )
+        .then(result => {
+          
+          res.json('Success')
+          
+          })
+        .catch(error => console.error(error))
+    })
+    
+
+
   })
   .catch(error => console.error(error))
-
-console.log('May Node be with you')
